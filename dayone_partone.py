408443929f1d13003fc642d860df293a27a6a9d3
vivialyn14 <77161978+vivialyn14@users.noreply.github.com>
2024-12-20 16:23:36 +0000

    Initial commit

---
+++ dayone_partone.py
@@ -0,0 +1,29 @@
+with open("puzzle_input.txt") as fh:
+  puzzle_input = fh.read()
+
+test_input = """3   4
+4   3
+2   5
+1   3
+3   9
+3   3"""
+
+left_list = []
+right_list = []
+index = 0
+answer = 0
+
+lines = puzzle_input.splitlines()
+for line in lines:
+  x, y = line.split()
+  left_list.append(int(x))
+  right_list.append(int(y))
+
+left_list.sort()
+right_list.sort()
+
+for i in left_list:
+  answer += abs(i - right_list[index])
+  index += 1
+
+print(answer)
